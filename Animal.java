f36f0d298337e54a8b40a066f3210c5e6f4c5959
nasipa1 <1abdyraiymova.n@gmail.com>
2025-02-12 16:24:28 +0600

    asfj;as

---
+++ Animal.java
@@ -0,0 +1,101 @@
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void eat() {
+        System.out.println("Eating..." );
+    }
+
+    public void makeSound() {
+        System.out.println("Make Sound..." );
+    }
+}
+class Dog extends Animal {
+    private String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void eat() {
+        System.out.println("the dog loves chicken bones");
+    }
+
+    public void makeSound() {
+        System.out.println("sabaka does ARRRRRRR");
+    }
+
+}
+
+class Cat extends Animal {
+    private boolean hasFur;
+    public Cat(String name, int age, boolean hasFur) {
+        super(name, age);
+        this.hasFur = hasFur;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public void eat() {
+        System.out.println("milk is for a cat");
+    }
+
+    public void makeSound() {
+        System.out.println("cat does meowmeowmeowmeow");
+    }
+}
+
+class Animals {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("monkey", 3);
+        Animal animal2 = new Dog("Aramzamzam", 12, "sabaka");
+        Animal animal3 = new Cat("Bars",  99, false);
+        animal1.makeSound();
+        animal2.makeSound();
+        animal3.makeSound();
+        animal1.eat();
+        animal2.eat();
+        animal3.eat();
+
+
+
+    }
+}
+
+
--- Book.java
+++ Book.java
@@ -1,108 +1,175 @@
 public class Book {
     private String title;
-    private String[] author;
-    private int numberOfPages;
+    private String author;
+    private int year;
 
-    public Book(){
-
-
-    }
-    public Book(String title, String author, int numberOfPages) {
-        this.title = title;
-//        this.author = author;
-        this.author = new String[2];
-        this.author[0] = author;
-        this.numberOfPages = numberOfPages;
-
-
-    }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String newTitle) {
-        this.title = newTitle;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getAuthor() {
-        return author[0];
+        return author;
     }
 
-    public void setAuthor(String author1, String author2) {
-        this.author[0] = author1;
-        this.author[1] = author2;
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
-    public String[] setAuthor() {
-        return this.author;
+    public int getYear() {
+        return year;
     }
 
-
-    public int getNumberOfPages() {
-        return numberOfPages;
+    public void setYear(int year) {
+        this.year = year;
     }
 
-    public void setNumberOfPages(int newNumberOfPages) {
-        this.numberOfPages = newNumberOfPages;
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+
+
+
+    }
+    public String getBookInfo() {
+        return "Title: " + title + "\nAuthor: " + author + "\nYear: " + year;
     }
 
-    public boolean equals(Book book) {
-        return getAuthor()==book.getAuthor() &&
-                getTitle()==(book.getTitle()) &&
-                getNumberOfPages() == book.getNumberOfPages();
+    public void printBookDetails(){
+        System.out.println(getBookInfo());
     }
-
-
 }
+class PrintedBookClass extends Book {
+    private int numOfPages;;
+    private String publisher;
+    private int price;
 
+    public PrintedBookClass(String title, String author, int year, int numOfPages, String publisher) {
+        super(title, author, year);
+        this.numOfPages = numOfPages;
+        this.publisher = publisher;
+        price = numOfPages * 2;
+    }
 
-class MainBook {
-    public static void main(String[] args) {
-        Book b1 = new Book();
-        Book b2 = new Book("Harry Potter", "J.K. Rowling", 700);
-        Book b3 = new Book("Harry Potter", "J.K. Rowling", 700);
-//        System.out.println("Info about b1:"+b1.getTitle() + " "+b1.getAuthor());
-//        System.out.println("Info about b2:"+b2.getTitle() + " "+b2.getAuthor());
-//
-        if (b2.equals(b3)) {
-            System.out.println("The book is the same");
+    public PrintedBookClass(String title, String author, int year, int numOfPages, String publisher, int price) {
+        super(title, author, year);
+        this.numOfPages = numOfPages;
+        this.publisher = publisher;
+        if (numOfPages > 300){
+            this.price = 700;
         } else {
-            System.out.println("The book is not the same");
+            this.price = 300;
         }
+    }
 
-        if (b2 == b3) {
-            System.out.println("The book is the same");
-        } else {
-            System.out.println("The book is not the same");
-        }
+    public int getNumOfPages() {
+        return numOfPages;
+    }
 
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    public String getBookInfo() {
+        return "Title:" + getTitle() + "\nAuthor:" + getAuthor() + "\nYear:" + getYear() + "\nNum of pages: " + numOfPages + "\nPublisher: " + publisher + "\nPrice: " + price;
+    }
+    public void printBookDetails(){
+        System.out.println(getBookInfo());
+    }
+
+    public void bookType(){
+        System.out.println("this is a printed book");
+    }
+
+    public void printBook(){
+        System.out.println("printing a book.......");
     }
 }
 
+class EBook extends Book{
+    private double fileSizeMB;
+    private String fileFormat;
+    private double price;
+    public EBook(String title, String author, int year, double fileSizeMB, String fileFormat) {
+        super(title, author, year);
+        this.fileSizeMB = fileSizeMB;
+        this.fileFormat = fileFormat;
+        price = fileSizeMB + 500;
+    }
 
-class Calculator{
-    public Calculator(){
+    public EBook(String title, String author, int year, double fileSizeMB, String fileFormat, double price) {
+        super(title, author, year);
+        this.fileSizeMB = fileSizeMB;
+        this.fileFormat = fileFormat;
+        this.price = price;
+    }
 
+    public double getFileSizeMB() {
+        return fileSizeMB;
     }
-    public static void add(int a, int b){
-        System.out.println(a+b);
+
+    public void setFileSizeMB(double fileSizeMB) {
+        this.fileSizeMB = fileSizeMB;
     }
-    public static void add(int c, double d){
-        System.out.println(c+d);
+
+    public String getFileFormat() {
+        return fileFormat;
     }
-    public static void add(double e, double f){
-        System.out.println(e+f);
+
+    public void setFileFormat(String fileFormat) {
+        this.fileFormat = fileFormat;
     }
+
+    public String getBookInfo() {
+        return "Title:" + getTitle() + "\nAuthor:" + getAuthor() + "\nYear:" + getYear() + "\nFile size: " + fileSizeMB + "MB\nFile format: " + fileFormat + "\nPrice: " + price;
+    }
+    public void printBookDetails(){
+        System.out.println(getBookInfo());
+    }
+    public void bookType(){
+        System.out.println("this is an EBook");
+    }
+
+
 }
 
-class DemoCalculator{
+class BookClass{
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        cal.add(42,35);
-        cal.add(21,2.3);
-        cal.add(32.1, 32.4);
+        System.out.println("Printed book info:\n");
+        System.out.println("Printed Book 1:\n");
+        PrintedBookClass b1 = new PrintedBookClass("1984", "George Orwell", 1949, 328,"Secker & Warburg");
+        b1.printBook();
+        b1.printBookDetails();
+        b1.bookType();
+
+
+        System.out.println("\nPrinted Book 2:\n");
+        PrintedBookClass b2 = new PrintedBookClass("Things Fall Apart", "Chinua Achebe", 1958, 224, "Penguin Books", 0);
+        b2.printBook();
+        System.out.println(b2.getBookInfo());
+        b2.bookType();
+
+        System.out.println("\nEBook Details:\n");
+        System.out.println("EBook 1:\n");
+        EBook b3 = new EBook("The Trial", "Franz Kafka", 1925, 75, "PDF" );
+        b3.printBookDetails();
+        b3.bookType();
+
+        System.out.println("\nEBook 2:\n");
+        EBook b4 = new EBook("Notre Dame De Paris", "Victor Hugo", 1831, 100, "PDF", 700);
+        System.out.println(b4.getBookInfo());
+        b4.bookType();
     }
 }
-
-
